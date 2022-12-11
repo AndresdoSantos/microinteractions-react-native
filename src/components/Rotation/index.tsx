3feada2f1f58bf2c312636e7fa9be05f1ec51d11
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-12-11 15:58:28 -0300

    feat(App): adding rotation animation

---
+++ src/components/Rotation/index.tsx
@@ -0,0 +1,28 @@
+import { View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+
+import { styles } from './styles'
+
+export function Rotation() {
+  const rotation = useSharedValue(0)
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotateZ: `${(rotation.value / Math.PI) * 180}deg` }]
+  }))
+
+  const onRotationGesture = 
+    Gesture
+      .Rotation()
+      .onUpdate(event => {
+        rotation.value = event.rotation
+      })
+
+  return (
+    <View style={styles.container}>
+      <GestureDetector gesture={onRotationGesture}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </GestureDetector>
+    </View>
+  )
+}
---
+++ src/components/Rotation/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2C2C2C'
+  },
+  box: {
+    height: 200,
+    width: 200,
+    borderRadius: 24,
+    backgroundColor: '#8527E5'
+  }
+})
